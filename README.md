6845fddb527229f70e75181a3c200b3fc10d84d9
jrdnwlkr <walker.jordan.m@gmail.com>
2023-04-20 02:20:37 -0500

    added bulk of js

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Sources</h1>
+<p><a href="https://www.programiz.com/javascript/online-compiler/?ref=2374e750">JavaScript Program to Generate Random String</a></p>
+<p><a href="https://www.w3schools.com/jsref/met_win_prompt.asp">Window prompt</a></p>
--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-// Assignment code here
+// // Assignment code here
+var uppercase = 'ABCDEFGHIHKLMNOPQRSTUVWXYZ';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var special = '!@#$%^&*';
+var numerical = '1234567890';
+var characters = uppercase + lowercase + special + numerical;
+
+console.log(uppercase, lowercase, special, numerical);
 
 
-// Get references to the #generate element
+function generateString(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  
+  return(result);
+
+}
+
+function selectCriteria() {
+  let length = prompt("What do you want the length of your password to be?");
+
+  if (length != null) {
+    console.log(length);
+    let types = prompt("What character types are allowed?");
+
+  if (types != null) {
+    let charactersArray = types.split(" ");
+    console.log(charactersArray);
+    console.log(charactersArray.length);
+    var characters = (charactersArray.join(' + '));
+    console.log(characters);
+    }
+  }
+  console.log(generateString(length));
+}
+
+// // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+
+  var criteria = selectCriteria()
+
+  return criteria
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  document.querySelector("#password").innerHtml = passwordText;
 
   passwordText.value = password;
 
